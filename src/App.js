60f4e000d3eae273ad49691aba764cade3af9e6a
DungNo <ducdungcntta@gmail.com>
2022-03-15 21:53:24 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dung Nobita
         </p>
         <a
           className="App-link"
